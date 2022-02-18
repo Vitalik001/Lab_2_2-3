80bd3b8db840433d89060bd099e4e9515866b151
Vitalii Petrychko <i.petrychko@ucu.edu.ua>
2022-02-18 10:47:30 +0200

    completed task 1

--- task_1.py
+++ task_1.py
@@ -2,20 +2,29 @@ import json
 def find_value(path):
     with open(path, encoding='utf-8') as file:
         data=json.load(file)
-    # print(data)
-    value=None
-    while type(data)==dict or type(data)==list:
-        if type(data)==dict:
-            print('Available keys: ', data.keys())
+    store=[]
+    store.append(data)
+    while len(store)!=0:
+        if type(store[-1])==dict:
+            print('Available keys: ', store[-1].keys())
             key=input()
-            value=data[key]
-            data=value
+            if key=='.':            
+                store.pop()                 
+                continue
+            store.append(store[-1][key])
             continue
-        if type(data)==list:
-            print('Available indexes: ', len(data))
-            index=int(input())
-            value=data[index]
-            data=value
-    print(value)
+        if type(store[-1])==list:
+            print('Available indexes: ', len(store[-1]))
+            index=input()
+            if index=='.':
+                store.pop()
+                continue
+            index=int(index)-1
+            store.append(store[-1][index])
+            continue
+        print(store[-1])
+        command=input()
+        if command=='.':
+            store.pop()
 if __name__=='__main__':
     find_value('/Users/vitalii/Desktop/Programming/2_semester/Lecture2/lab_2/twitter2.json')
